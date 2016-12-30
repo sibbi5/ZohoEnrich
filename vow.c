37036b7fc99d6dca3a51ccf025b788bae270a34d
sibbi5 <jothisibbi@gmail.com>
2016-12-30 20:01:19 +0530

    Rename vov.c to vow.c

---
+++ vow.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char word[50],ch;
+    int n,i,j=0,k=0;
+    ch=getchar();
+    while (ch != '\n')
+    {
+        word[k++]=ch;
+        ch=getchar();
+    }
+
+    for(i=0;i<k;i++)
+    {
+        if(word[i]!='a' && word[i]!='e' && word[i]!='i' && word[i]!='o' && word[i]!='u' && word[i]!=' ' && word[i]!='A' && word[i]!='E' && word[i]!='I' && word[i]!='O' && word[i]!='U' && ((word[i]>65&&word[i]<90)||(word[i]>97&&word[i<122])) )
+        {
+        word[j]=word[i];
+        j++;
+        }
+
+    }
+    for(i=0;i<j;i++)
+        printf("%c",word[i]);
+
+}
+
+
