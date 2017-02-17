32d08e8fc09627d3a0e9d5e94bcce8d21fd570ae
sibbi5 <jothisibbi@gmail.com>
2017-02-17 14:27:06 +0530

    Update epdinetherila.c

--- epdinetherila.c
+++ epdinetherila.c
@@ -11,22 +11,29 @@ void main()
 		num[i]=(char*)malloc(10*sizeof(char));
 	}
     char* temp;
+    char* temp1;
 	for(i=0;i<n;i++)
 		scanf("%s", num[i]);
 	for(i=0;i<n-1;i++)
 	{
 		temp=num[i];
+		k=0;
 		for(j=i+1;j<n;j++)
 		{
 			if(strcmp(temp,num[j])>0)
 			{
 				temp=num[j];
+				k=j;
 			}
-			
+			if(k==0)
+			{
+			    k=i;
+			}
 		}
-		
-	num[k++]=temp;	
-	}		
+	temp1=num[i];
+	num[i]=temp;
+	num[k]=temp1;
+	}
 	for(i=0;i<n;i++)
 	{
 		printf("%s", num[i]);
